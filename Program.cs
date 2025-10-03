0623ae8cb6f8432fa4e5573da342f547753d7e96
Alirie Gray <alirie.gray@gmail.com>
2025-10-03 16:29:15 -0700

    attempt 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
+using WorldModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
+<<<<<<< HEAD:Program.cs
 // Add services to the container
 
+=======
+// Add services to the container.
+>>>>>>> 9ae84d5 (add countries controller):Comp584Server/Program.cs
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+<<<<<<< HEAD:Program.cs
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+=======
+>>>>>>> 9ae84d5 (add countries controller):Comp584Server/Program.cs
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<WeatherContext>();
 
 var app = builder.Build();
 
