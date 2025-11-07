47a0c9f998950913f8f208527a4da9a4873ff94d
cnguye-n <52482302+cnguye-n@users.noreply.github.com>
2025-11-07 11:43:33 -0800

    updated Server

---
+++ CSUN584Server/Data/DTOs/CountryPopulation.cs
@@ -0,0 +1,15 @@
+namespace CSUN584Server.Data.DTOs
+{
+    public class CountryPopulation
+    {
+        public required int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Iso2 { get; set; }
+
+        public required string Iso3 { get; set; }
+
+        public int Population { get; set; }
+    }
+}
